440e6ca543b863016103f459337ef7cbc6cd2ffc
Tommy Ma <124090467@link.cuhk.edu.cn>
2026-01-12 17:14:57 +0800

    done with A1

--- imageloader.c
+++ imageloader.c
@@ -26,16 +26,62 @@
 Image *readData(char *filename) 
 {
 	//YOUR CODE HERE
+	FILE *fp = fopen(filename, "r");
+	char form[5];
+	int row, col, scale;
+	fscanf(fp, "%s %d %d %d", form, &col, &row, &scale);
+	Color **color = malloc(sizeof(Color *) * row);
+
+	for (int i = 0; i < row; ++i) {
+		color[i] = malloc(sizeof(Color) * col);
+		for (int j = 0; j < col; ++j) {
+			int r, g, b;
+			fscanf(fp, "%d %d %d", &r, &g, &b);
+			color[i][j].R = r;
+			color[i][j].G = g;
+			color[i][j].B = b;
+		}
+	}
+
+	Image *image = malloc(sizeof(Image));
+	image->rows = row;
+	image->cols = col;
+	image->image = color;
+
+	fclose(fp);
+	return image;
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
 	//YOUR CODE HERE
+	printf("P3\n%d %d\n255\n", image->cols, image->rows);
+	for (int i = 0; i < image->rows; ++i) {
+		for (int j = 0; j < image->cols; ++j) {
+			Color p = image->image[i][j];
+
+            printf("%3d %3d %3d", p.R, p.G, p.B);
+			if (j != image->cols - 1) {
+				printf("   ");
+			}
+		}
+		printf("\n");
+	}
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
 	//YOUR CODE HERE
+	if (image == NULL) {
+		return;
+	}
+
+	for (int i = 0; i < image->rows; ++i) {
+		free(image->image[i]);
+	}
+
+	free(image->image);
+	free(image);
 }
